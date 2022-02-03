323c1cbcd3d7a42d585e8ef8168ce9740b68d031
raminjafary <raminjafary1993@gmail.com>
2022-02-03 19:07:39 +0330

    chore: add vue global macros to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    // 'vue/setup-compiler-macros': true
   },
   extends: [
     "plugin:vue/vue3-essential",
@@ -16,5 +17,9 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
   },
 };
